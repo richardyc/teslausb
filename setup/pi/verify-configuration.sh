99dd22b14d8ab84507de4ae13d3163914067d266
Richard He <he@hrichard.com>
2019-04-01 17:00:34 -0500

    Update verify-configuration.sh

--- setup/pi/verify-configuration.sh
+++ setup/pi/verify-configuration.sh
@@ -14,12 +14,6 @@ function check_available_space () {
 
   local available_space="$( parted -m /dev/mmcblk0 u b print free | tail -1 | cut -d ":" -f 4 | sed 's/B//g' )"
 
-  if [ "$available_space" -lt  4294967296 ]
-  then
-    setup_progress "STOP: The MicroSD card is too small."
-    exit 1
-  fi
-
   setup_progress "There is sufficient space available."
 }
 
